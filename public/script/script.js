9bac57d0699eac0639b129698e1a058b49651738
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-03-02 01:22:02 +0700

    Changed the logic to be more detail and understandable

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,11 @@
-let welcomeButton = document.querySelector("button");
+// Targeting the Element!
+let welcomeButton = document.querySelector("#loginButton");
 let dynamicTitle = document.querySelector("h1");
 
-welcomeButton.addEventListener("click", function(){
+//Adding logic into a function
+function loginForm () {
     dynamicTitle.innerHTML = "Welcome to the form!";
-})
+};
+
+//Adding listener to the element and call the function!
+welcomeButton.addEventListener("click", loginForm);
