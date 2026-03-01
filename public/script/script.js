a3f52c6c0266fc03f8cca65a3978ddd2a35d91b0
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-03-02 01:44:35 +0700

    Added animation into h1 after the button triggered

--- public/script/script.js
+++ public/script/script.js
@@ -5,6 +5,8 @@ let dynamicTitle = document.querySelector("h1");
 //Adding logic into a function
 function loginForm () {
     dynamicTitle.innerHTML = "Welcome to the form!";
+    dynamicTitle.classList.add('h1-after')
+
 };
 
 //Adding listener to the element and call the function!
