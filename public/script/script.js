706313c49bad9badfebde39210115d52ccedf5cd
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-03-02 14:14:28 +0700

    Added form logic and added link to other html file

--- public/script/script.js
+++ public/script/script.js
@@ -1,11 +1,37 @@
 // Targeting the Element!
 let welcomeButton = document.querySelector("#loginButton");
 let dynamicTitle = document.querySelector("h1");
+let formHidden = document.querySelector(".form-login")
+let notification = document.querySelector("#notif")
+let correctUsername = 'arifRahman';
+let correctPassword = 'arif123';
 
 //Adding logic into a function
 function loginForm () {
-    dynamicTitle.innerHTML = "Welcome to the form!";
-    dynamicTitle.classList.add('h1-after')
+
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
+    let reminder = notification
+
+    
+    if (username === correctUsername && password === correctPassword) {
+        notification.innerHTML = 'Login Succesfully! Redirecting...';
+        notification.style.color = 'green';
+
+        setTimeout(function () {
+            window.location.href = 'home.html';
+        }, 2000);
+        
+    } else if (username === '' || password === '') {
+        reminder.textContent = 'Field cannot be empty';
+        reminder.style.color = 'red'
+
+    } else if (username !== correctUsername || password === correctPassword) {
+        reminder.textContent = 'Username or Password incorrect';
+        reminder.style.color = 'red'
+    
+    }
+    
 
 };
 
